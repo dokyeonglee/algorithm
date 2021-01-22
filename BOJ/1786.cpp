b5b47b9bbb674e3908811178ab897924e055a4b5
dokyeonglee <dokyung.dev@gmail.com>
2021-01-22 14:51:49 +0900

    BOJ 1786 KMP

---
+++ BOJ/1786.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> failure_function(string s){
+	
+	vector<int> partial_match(s.size());
+	
+	int begin = 1;
+	int matched = 0;
+	
+	while(begin + matched < s.size()){
+		if(s[begin + matched] == s[matched]){
+			partial_match[begin + matched - 1] = ++matched;
+		}else{
+			if(matched == 0){
+				begin++;
+			}else{
+				begin += matched - partial_match[matched - 1];
+				matched = partial_match[matched - 1];
+			}
+		}
+	}
+	
+	return partial_match;
+}
+
+vector<int> kmp(string hay, string niddle){
+	
+	int n = hay.size();
+	int m = niddle.size();
+	
+	vector<int> result;
+	vector<int> partial_match = failure_function(niddle);
+	
+	int begin = 0;
+	int matched = 0;
+	
+	while(begin <= n - m){
+		if(matched < m and hay[begin + matched] == niddle[matched]){
+			if(++matched == m){
+				result.push_back(begin);
+			}
+		}else{
+			if(matched == 0){
+				begin++;
+			}else{
+				begin += matched - partial_match[matched - 1];
+				matched = partial_match[matched - 1];
+			}
+		}
+	}
+	return result;
+}
+
+int main() {
+	
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	
+	string T, P;
+	getline(cin, T);
+	getline(cin, P);
+	
+	vector<int> answer = kmp(T, P);
+	
+	cout << answer.size() << endl;
+	
+	for(int i = 0; i < answer.size(); i++){
+		cout << answer[i] + 1 << " ";
+	}
+	
+	cout << endl;
+	
+	return 0;
+}
