2d42bf00875a37f67742d190dc99697dc0c19953
dokyeonglee <dokyung.dev@gmail.com>
2021-07-23 03:00:49 +0900

    BOJ 1916 dijkstra

---
+++ BOJ/1916.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+int dijkstra(vector<vector<pair<int, int>>>& graph, int src, int dst){
+	
+	vector<int> distance(graph.size(), 987654321);
+	distance[src] = 0;
+	
+	priority_queue<pair<int, int>> pq;
+	pq.push(make_pair(0, src));
+	
+	while(!pq.empty()){
+		
+		int dist = -pq.top().first;
+		int now = pq.top().second;
+		pq.pop();
+		
+		if(distance[now] < dist){
+			continue;
+		}
+		
+		for(pair<int, int> e : graph[now]){
+			int next = e.first;
+			int cost = e.second + dist;
+			if(distance[next] > cost){
+				distance[next] = cost;
+				pq.push(make_pair(-cost, next));
+			}
+		}
+		
+	}
+	
+	return distance[dst];
+	
+}
+
+int main() {
+	
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	
+	int n, m;
+	cin >> n >> m;
+	
+	vector<vector<pair<int, int>>> graph(n + 1);
+	
+	for(int i = 0; i < m; i++){
+		int u, v, c;
+		cin >> u >> v >> c;
+		graph[u].push_back(make_pair(v, c));
+	}
+	
+	int src, dst;
+	cin >> src >> dst;
+	
+	cout << dijkstra(graph, src, dst);
+	
+	return 0;
+}
