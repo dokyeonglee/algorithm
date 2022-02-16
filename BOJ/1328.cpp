301a4bbdde85dc25637662595b4ffc7dbee93443
dokyung <dokyung.dev@gmail.com>
2022-02-16 22:20:04 +0900

    BOJ 1328 dp

--- BOJ/1328.cpp
+++ BOJ/1328.cpp
@@ -11,23 +11,10 @@ int main() {
 	cin >> n >> l >> r;
 	
 	dp[1][1][1] = 1;
-	dp[2][1][2] = 1;
-	dp[2][2][1] = 1;
 	
-	for(int k = 3; k <= n; k++){
+	for(int k = 2; k <= n; k++){
 		for(int i = 1; i <= k; i++){
-			if(i == k){
-				dp[k][i][1] = 1;
-				continue;
-			}
 			for(int j = 1; j <= k; j++){
-				if(i == 1 and j == 1){
-					continue;
-				}
-				if(j == k){
-					dp[k][1][j] = 1;
-					continue;
-				}
 				dp[k][i][j] = dp[k - 1][i][j] * (k - 2)
 							+ dp[k - 1][i - 1][j]
 							+ dp[k - 1][i][j - 1];
