9341029ff1c2f8af8834cf0ce7022aacabac1e98
dokyung <dokyung.dev@gmail.com>
2022-02-17 23:59:16 +0900

    BOJ 13707 dp

--- BOJ/13707.cpp
+++ BOJ/13707.cpp
@@ -5,21 +5,20 @@ using namespace std;
 
 int main() {
 	
-	vector<vector<long long>> C(10005, vector<long long>(5005));
-	C[1][0] = 1;
-	C[1][1] = 1;
-	
-	for(int i = 2; i <= 10001; i++){
-		C[i][0] = 1;
-		for(int j = 1; j <= 5001; j++){
-			C[i][j] = (C[i - 1][j - 1] + C[i - 1][j]) % 1000000000;
-		}
-	}
+	vector<long long> C(10005);
+	C[1] = 1;
+	C[0] = 1;
 	
 	int n, k;
 	cin >> n >> k;
 	
-	cout << C[n + k - 1][n];
+	for(int i = 2; i <= n + k - 1; i++){
+		for(int j = i; j >= 1; j--){
+			C[j] = (C[j - 1] + C[j]) % 1000000000;
+		}
+	}
+	
+	cout << C[n];
 	
 	return 0;
 }
