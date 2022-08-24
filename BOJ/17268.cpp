cb78bdfae87aeded0645e10323b24f27a90a7773
dokyung <dokyung.dev@gmail.com>
2022-08-24 12:55:37 +0900

    BOJ 17268 dp

--- BOJ/17268.cpp
+++ BOJ/17268.cpp
@@ -8,11 +8,11 @@ int main() {
 	int n;
 	cin >> n;
 	
-	vector<long long> catalan(n + 1);
+	vector<long long> catalan(n / 2 + 1);
 	catalan[0] = 1;
 	catalan[1] = 1;
 	
-	for(int i = 1; i < n; i++){
+	for(int i = 1; i < n / 2; i++){
 		for(int j = 0; j <= i; j++){
 			catalan[i + 1] = (catalan[i + 1] + catalan[i - j] * catalan[j]) % 987654321;
 		}
