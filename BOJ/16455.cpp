0fc96524c99f20883f360e864dfb2404b8f3c159
dokyung <dokyung.dev@gmail.com>
2022-09-24 13:43:42 +0900

    BOJ 16455 etc

--- BOJ/16455.cpp
+++ BOJ/16455.cpp
@@ -1,26 +1,30 @@
 #include <vector>
+#include <algorithm>
 #include <iostream>
 
 using namespace std;
 
 int partition(vector<int>& arr, int left, int right){
 	
-	int random_idx = left + rand() % (right - left);
+	int random_idx = left + rand() % (right - left + 1);
 	swap(arr[random_idx], arr[right]);
 	
 	int pivot = arr[right];
-	int i = left;
+	int i = left - 1;
 	
 	for(int j = left; j < right; j++){
-		if(arr[j] < pivot){
-			swap(arr[i], arr[j]);
-			i++;
+		if(arr[j] <= pivot){
+            int temp = arr[++i];
+            arr[i] = arr[j];
+            arr[j] = temp;
 		}
 	}
 	
-	swap(arr[right], arr[i]);
-	
-	return i;
+    int temp = arr[right];
+    arr[right] = arr[i + 1];
+    arr[i + 1] = temp;
+    
+	return i + 1;
 	
 }
 
@@ -29,18 +33,29 @@ int quick_select(vector<int>& arr, int left, int right, int k){
 		return arr[left];
 	}
 	int idx = partition(arr, left, right);
-	if(idx == k){
-		return arr[k];
-	}else if(idx > k){
+	if(idx - left + 1 == k){
+		return arr[idx];
+	}else if(idx - left + 1 > k){
 		return quick_select(arr, left, idx - 1, k);
 	}else{
-		return quick_select(arr, idx + 1, right, k);
+		return quick_select(arr, idx + 1, right, k - (idx - left + 1));
 	}
 }
 
 int kth(vector<int> &a, int k) {
-    k--;
-    k = a.size() - k;
-	int ans = quick_select(a, 0, a.size() - 1, a.size() - k);
+	if(is_sorted(a.begin(), a.end())){
+		return a[k - 1];
+	}
+	bool check = true;
+	for(int i = 0; i < a.size(); i++){
+		if(a[i] != a[0]){
+			check = false;
+			break;
+		}
+	}
+	if(check){
+		return a[0];
+	}
+	int ans = quick_select(a, 0, a.size() - 1, k);
 	return ans;
 }
