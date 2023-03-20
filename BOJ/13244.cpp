69f98f0749ed9c04c7ef42fa64882ef1fb3cd05c
dokyung <dokyung.dev@gmail.com>
2023-03-21 06:49:01 +0900

    BOJ 13244 disjoint set

---
+++ BOJ/13244.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int find_parent(vector<int>& parent, int x){
+    if(parent[x] == x){
+        return x;
+    }
+    return parent[x] = find_parent(parent, parent[x]);
+}
+
+void union_parent(vector<int>& parent, int x, int y){
+    x = find_parent(parent, x);
+    y = find_parent(parent, y);
+    parent[y] = x;
+}
+
+int main() {
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int t;
+    cin >> t;
+
+    while(t--){
+
+        int n, m;
+        cin >> n >> m;
+
+        vector<int> parent(n + 1);
+        for(int i = 1; i <= n; i++){
+            parent[i] = i;
+        }
+
+        bool check = true;
+
+        for(int i = 0; i < m; i++){
+            int u, v;
+            cin >> u >> v;
+            if(find_parent(parent, u) != find_parent(parent, v)){
+                union_parent(parent, u, v);                
+            }else{
+                check = false;
+            }
+        }
+
+        int cnt = 0;
+        for(int i = 1; i <= n; i++){
+            if(find_parent(parent, i) == i){
+                cnt++;
+            }
+        }
+
+        if(cnt != 1){
+            check = false;
+        }
+
+        if(check){
+            cout << "tree\n";
+        }else{
+            cout << "graph\n";
+        }
+
+    }
+
+    return 0;
+}
