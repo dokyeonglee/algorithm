894bc17861cd5cf8fb1afcafd8b0ebc5a7125b97
dokyeonglee <dokyung.dev@gmail.com>
2026-01-03 15:49:15 +0900

    BOJ 2631 lis

---
+++ BOJ/2631.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	
+	int n;
+	cin >> n;
+	
+	vector<int> v{-1};
+	for (int i = 0; i < n; i++) {
+		int x;
+		cin >> x;
+		if (v.back() < x) {
+			v.push_back(x);
+		} else {
+			auto it = lower_bound(v.begin(), v.end(), x);
+			*it = x;
+		}
+	}
+	
+	cout << n - (v.size() - 1);
+	
+	return 0;
+}
