75a4374bf6c07322e78c8375605d6136bd533672
dokyeonglee <dokyung.dev@gmail.com>
2026-01-13 13:28:42 +0900

    BOJ 35112 etc

---
+++ BOJ/35112.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+
+    if (m <= n) {
+        cout << "Yes\n";
+    } else {
+        cout << "No\n";
+    }
+
+    return 0;
+}
