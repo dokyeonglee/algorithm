b8549af57a02e72065f5208eab3aa124bba48b90
dokyeonglee <dokyung.dev@gmail.com>
2026-02-16 17:18:32 +0900

    BOJ 2327 dp

---
+++ BOJ/2327.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int h, n;
+    cin >> h >> n;
+
+    vector<pair<int, int>> students(n);
+    for (auto& [height, speed] : students) {
+        cin >> height >> speed;
+    }
+
+    vector<int> dp(h + 1);
+    dp[0] = 1e9;
+    for (auto& [height, speed] : students) {
+        for (int i = h; i >= height; --i) {
+            if (dp[i - height])
+            dp[i] = max(dp[i], min(dp[i - height], speed));
+        }
+    }
+
+    cout << dp[h] << '\n';
+
+    return 0;
+}
