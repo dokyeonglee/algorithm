38c1158c595a71ed6a4733909f7d420a06596288
dokyeonglee <dokyung.dev@gmail.com>
2026-03-06 11:01:53 +0900

    BOJ 17080 etc

---
+++ BOJ/17080.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    if (n % 4 == 3) {
+        cout << 2;
+    } else {
+        cout << 1;
+    }
+
+    return 0;
+}
