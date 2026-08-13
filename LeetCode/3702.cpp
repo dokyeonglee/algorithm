84de5261879ee74d9d0580bbab834c4816411254
dokyeonglee <dokyung.dev@gmail.com>
2026-08-13 13:11:16 +0900

    LeetCode 3702 etc

---
+++ LeetCode/3702.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int longestSubsequence(vector<int>& nums) {
+        bool check = true;
+        int x = 0;
+        for (int num : nums) {
+            if (num) {
+                check = false;
+            }
+            x ^= num;
+        }
+
+        if (check) {
+            return 0;
+        }
+
+        return nums.size() - (x == 0);
+    }
+};
