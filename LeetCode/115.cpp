341cd66cb860f1ce2b27040aab1314471d2d33a9
dokyeonglee <dokyung.dev@gmail.com>
2026-09-06 21:36:59 +0900

    LeetCode 115 dp

---
+++ LeetCode/115.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int numDistinct(string s, string t) {
+        if (t.size() > s.size()) {
+            return 0;
+        }
+
+        vector<unsigned long long> dp(t.size() + 1);
+        dp[t.size()] = 1;
+
+        for (int i = s.size() - 1; i >= 0; --i) {
+            for (int j = 0; j < t.size(); ++j) {
+                if (s[i] == t[j]) {
+                    dp[j] += dp[j + 1];
+                }
+            }
+        }
+
+        return dp[0];
+    }
+};
